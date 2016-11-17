a4deebea4db43564966c51a11506254acb851d8c
Lesya13 <Lesya@MacBook-Air-Lesya.local>
2016-11-18 00:08:01 +0300

    финальная проверка

--- js/script.js
+++ js/script.js
@@ -1,84 +1,62 @@
-var link = document.querySelector(".address-btn"),
-    popup = document.querySelector(".feedback"),
-    close = document.querySelector(".js-btn-close"),
-    form = popup.querySelector("form"),
-    lining = popup.querySelector(".lining"),
-    login = popup.querySelector("[name=login]"),
-    email = popup.querySelector("[name=email]"),
-    text = popup.querySelector("[name=text]"),
-    storageName = localStorage ? localStorage.getItem("login") : "",
-    storageEmail = localStorage ? localStorage.getItem("email") : "";
+//обратная связь
+var link = document.querySelector(".address-btn");
+var popup = document.querySelector(".feedback");
+var close = document.querySelector(".js-btn-close");
+var overlay = document.querySelector(".feedback-overlay");
+var submit = popup.querySelector(".feedback-btn");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+var storage_user = localStorage.getItem("login");
+var storage_email = localStorage.getItem("email");
 
-
-// Функция сброса классов (для закрытия модального окна)
-var exit = function (event) {
-    event.preventDefault();
-    popup.classList.remove("feedback-show");
-    form.classList.remove("feedback-bounce");
-    form.classList.remove("feedback-error");
-};
-
-// Открытие модального окна и действия в нём и с ним
-link.addEventListener("click", function (event) {
-    event.preventDefault();
-    popup.classList.add("feedback-show");
-    form.classList.add("feedback-bounce");
-
-    if (storageName && storageEmail) {
-        login.value = storageName;
-        email.value = storageEmail;
-        text.focus();
-    } else {
-        login.focus();
-    };
-
-    // Дрожание модального окна при попытке отправки незаполненной формы и запись значений в localStorage валидных имени и почты
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        if (!login.value || !email.value || !text.value) {
-            form.classList.remove("feedback-error");
-            form.offsetWidth = form.offsetWidth;
-            form.classList.add("feedback-error");
-        } else if (localStorage) {
-            localStorage.setItem("login", login.value);
-            localStorage.setItem("email", email.value);
-        }
-    });
-
-    // Закрытие модального окна по клику вне его и по крестику
-    window.addEventListener("keydown", function (event) {
-        if (event.keyCode === 27) {
-            if (popup.classList.contains("feedback-show")) {
-                exit(event);
-            }
-        }
-    });
-    
-        close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("feedback-show");
-        popup.classList.remove("lining");
-        popup.classList.remove("feedback-error");
-      });
-    
-    
-    
-    lining.addEventListener("click", exit, true);
-    close.addEventListener("click", exit, true);
+link.addEventListener("click", function (a) {
+a.preventDefault();
+popup.classList.add("feedback-show");
+overlay.classList.add("feedback-overlay-show");
+storage_user && storage_email ? (login.value = storage_user, email.value = storage_email, text.focus()) : login.focus();
 });
+
+close.addEventListener("click", function (a) {
+a.preventDefault();
+popup.classList.remove("feedback-show");
+popup.classList.remove("feedback-error");
+overlay.classList.remove("feedback-overlay-show");
+});
+
+submit .addEventListener("click", function (a) {
+login.value && email.value && text.value ? (localStorage.setItem("login", login.value), localStorage.setItem("email", email.value)) : (a.preventDefault(), popup.classList.remove("feedback-error"),popup.offsetWidth = popup.offsetWidth, popup.classList.add("feedback-error"));
+});
+
+window.addEventListener("keydown", function (a) {
+27 === a.keyCode && popup.classList.contains("feedback-show") && (popup.classList.remove("feedback-show"), popup.classList.remove("feedback-error"), overlay.classList.remove("feedback-overlay-show"));
+});
+
+overlay.addEventListener("click", function (a) {
+popup.classList.contains("feedback-show") && (popup.classList.remove("feedback-show"), popup.classList.remove("feedback-error"), overlay.classList.remove("feedback-overlay-show"));
+});
+
+//Переменные окна "добавлено в корзину"
+var basket = document.querySelector(".modal-cart");
+var linksBuy = document.querySelectorAll(".product-popular__btn-buy");
+var basketClose = basket.querySelector(".js-close-cart");
+var continueShop = basket.querySelector(".modal-cart__btn--js");
 
 // Настройка карты
 function initMap() {
-  var mapDiv = document.querySelector('.google-maps');
-  var myLatLng = {lat: 59.938810, lng: 30.323400};
-  var map = new google.maps.Map(mapDiv, {
-    center: myLatLng,
-    zoom: 15
-  });
- var image = "img/map-marker.png";
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    icon: image
-  });
+    var mapDiv = document.querySelector('.google-maps');
+    var myLatLng = {
+        lat: 59.938810,
+        lng: 30.323400
+    };
+    var map = new google.maps.Map(mapDiv, {
+        center: myLatLng,
+        zoom: 15
+    });
+    var image = "img/map-marker.png";
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
 }
